04d8dfd0b6d28223d7499df52cce2b4dd32f3ffa
arhahrear30 <arshahrear30@gmail.com>
2025-05-13 03:07:08 +0600

    Update bdandphp.php

--- bdandphp.php
+++ bdandphp.php
@@ -1,9 +1,13 @@
 <?php
 
+//প্রথমে server এর Hosting  এর C pannel এ Database এ যাবো ।সেখানে Manage Database এ যাবো 
+//তারপর  Add User To Database এখানে New User/New Database/DBUSERID & DBPassword set করবো । তারপর Database 
+//create করবো ।  এরপর phpmyadmin এ যাবো ।
 
 //mysqli_connect(); এর কাজ database(phpmyadmin) এর সাথে php file এর connection ঘটানো
 // mysqli_connect(); এর ভিতর 4 টা part থাকবে । 1st part DB(database) localhost 
 //mysqli_connect("localhost","DBUSERID","DBPassword","DBNameinphpmyadmin");
+
 mysqli_connect("localhost","zftsszne_shahrear","arshahrear30@gmail.com","zftsszne_my_database");
 
 if(mysqli_connect_errno()){
