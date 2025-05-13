73c2d9d7ed42747e5ec31f0de1a9ee4910551281
arhahrear30 <arshahrear30@gmail.com>
2025-05-13 23:03:19 +0600

    Update 1 bdandphp.php

--- bdandphp.php
+++ bdandphp.php
@@ -7,8 +7,8 @@
 //mysqli_connect(); এর কাজ database(phpmyadmin) এর সাথে php file এর connection ঘটানো
 // mysqli_connect(); এর ভিতর 4 টা part থাকবে । 1st part DB(database) localhost 
 //mysqli_connect("localhost","DBUSERID","DBPassword","DBNameinphpmyadmin");
-
-mysqli_connect("localhost","zftsszne_shahrear","arshahrear30@gmail.com","zftsszne_my_database");
+//dollar sign দিয়ে variable ধরতে হয় । এখানে con একটা variable 
+$con = mysqli_connect("localhost","zftsszne_shahrear","arshahrear30@gmail.com","zftsszne_my_database");
 
 if(mysqli_connect_errno()){
 	//mysqli_connect_errno এটার মানে , যদি mysql এর সাথে connect না হয় তাহলে show করবে Couldn't connect to database এবং<br> মানে newline
@@ -17,6 +17,14 @@ if(mysqli_connect_errno()){
 }else{
 	echo"Connected with database";
 }
+
+//variable ধরলাম sql তারপর Database(phpmyadmin) এ data insert  করে Go তে দিলে Query গুলো Show করবে । 
+$sql = "INSERT INTO user_table (name,mobile,email)VALUES ('John','676','john40')";
+$result = mysqli_query($con,$sql);
+//mysqli_query মানে server এর সাথে connection ঘটানো । আর 1st bracket  মাঝে বুঝাইলাম কার কার সাথে connection করলাম
+
+if($result) echo"<br> Data insert Successful" ;// যদি সঠিক ভাব result variable এর কাজ হয় তখন Successful echo হবে ।
+else echo " Query error!!";
 
 
 ?>
